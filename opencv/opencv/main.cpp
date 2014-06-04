cef4573e54c512e1df231df3ad98eb148e599525
Yasir <berlinhitler@gmail.com>
2014-06-04 16:36:59 +0930

    Modified testing application

--- opencv/opencv/main.cpp
+++ opencv/opencv/main.cpp
@@ -177,8 +177,8 @@ int main(int argc, char* argv[])
 {
 	//some boolean variables for different functionality within this
 	//program
-    bool trackObjects = true;
-    bool useMorphOps = true;
+    bool trackObjects = false;
+    bool useMorphOps = false;
 	//Matrix to store each frame of the webcam feed
 	Mat cameraFeed;
 	//matrix storage for HSV image
